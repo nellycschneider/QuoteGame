1b746aa652a4438a48708c042777734c7009345b
Nelly-Charlott Schneider <nelly@Nelly-Charlotts-MacBook-Pro.local>
2019-08-26 12:48:58 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,29 @@
+let quotes = [
+  {
+    title: "marcus"
+  },
+  {
+    title: "frozone"
+  },
+  {
+    title: "drEvil"
+  }
+];
+
+let quoteReference = [
+  {
+    keyword: "marcus",
+    quote:
+      "The happiness of your life depends upon the quality of your thoughts."
+  },
+  {
+    keyword: "frozone",
+    quote:
+      "Greater good?’ I am your wife! I’m the greatest good you’re ever gonna get!"
+  },
+  {
+    keyword: "drEvil",
+    quote:
+      "We get the warhead and we hold the world ransom for… One million dollars!"
+  }
+];
