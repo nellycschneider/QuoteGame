67c2bbe38382dc647837f47fcbf2eb172ad233e8
Nelly-Charlott Schneider <nelly@Nelly-Charlotts-MacBook-Pro.local>
2019-08-30 10:07:34 +0200

    created a second js file for all the quotes/data

---
+++ data.js
@@ -0,0 +1,292 @@
+//change the name to data
+let quotes = [
+  // {
+  //   quote:
+  //     "Greater good? I am your wife! I’m the greatest good you’re ever gonna get!",
+  //   answers: [
+  //     "Fiona",
+  //     "Marge Simpson",
+  //     "Maude Flanders",
+  //     "Frozones' wife",
+  //     "Cinderella",
+  //     "Kim Kardashian"
+  //   ],
+  //   correct: "Frozones' wife"
+  // },
+  {
+    quote:
+      "We get the warhead and we hold the world ransom for… One million dollars!",
+    answers: [
+      "King Arthur",
+      "Will Ferrell",
+      "Al Capone",
+      "Dr. Evil",
+      "Austin Powers",
+      "Rick Sanchez"
+    ],
+    correct: "Dr. Evil"
+  },
+  {
+    quote:
+      "It’s not a man purse. It’s called a satchel. Indiana Jones wears one.",
+    answers: [
+      "Kevin Hart",
+      "Alan Garner",
+      "Z. Galifianakis",
+      "Homer Simpson",
+      "Charlie Sheen",
+      "Deadpool"
+    ],
+    correct: "Alan Garner"
+  },
+  {
+    quote: "The fast way is to drop thermonuclear weapons over the poles.",
+    answers: [
+      "Elon Musk",
+      "The Joker",
+      "Dr. Evil",
+      "Voldemort",
+      "Freddy Krueger",
+      "Queen of Hearts"
+    ],
+    correct: "Elon Musk"
+  },
+  {
+    quote: "Keep your friends close, but your enemies closer.",
+    answers: [
+      "Hannibal Lecter",
+      "The Joker",
+      "The Godfather",
+      "Blackbeard",
+      "Cruella De Vil",
+      "Eric Cartman"
+    ],
+    correct: "The Godfather"
+  },
+  {
+    quote: "Sub Saharan, can you have 150 child warriors here by 5:00pm?",
+    answers: [
+      "Will Smith",
+      "General Hopper",
+      "Bender",
+      "Admiral Aladeen",
+      "Jack Sparrow",
+      "S. Baron Cohen"
+    ],
+    correct: "Admiral Aladeen"
+  },
+  {
+    quote:
+      "Wikipedia is the best thing ever. Anyone in the world can write anything they want about any subject. So you know you are getting the best possible information.",
+    answers: [
+      "S. Baron Cohen",
+      "Dwight Schrute",
+      "Phil Dunphy",
+      "Michael Scott",
+      "Chandler Bing",
+      "Cosmo Kramer"
+    ],
+    correct: "Michael Scott"
+  },
+  {
+    quote:
+      "Before I do anything, I ask myself, 'Would an idiot do that?' And, if the answer is yes, I do not do that thing.",
+    answers: [
+      "Dwight Schrute",
+      "Sheldon Cooper",
+      "Michael Scott",
+      "Danny DeVito",
+      "Peter Parker",
+      "Butters Scotch"
+    ],
+    correct: "Dwight Schrute"
+  },
+  {
+    quote:
+      "That's it, dishonor! Dishonor on your whole family! Dishonor on you, dishonor on your cow!",
+    answers: [
+      "Spongebob",
+      "Mushu",
+      "Frodo Baggins",
+      "Eddie Murphy",
+      "Danny DeVito",
+      "Han Solo"
+    ],
+    correct: "Mushu"
+  },
+  {
+    quote:
+      "When life gives you lemonade, make lemons. Life will be all, ‘Whaaat?’",
+    answers: [
+      "Stewie Griffin",
+      "Michael Scott",
+      "Eric Cartman",
+      "Phil Dunphy",
+      "S. Baron Cohen",
+      "Peter Griffin"
+    ],
+    correct: "Phil Dunphy"
+  },
+  {
+    quote: "Cool, cool, cool, cool, cool. No doubt, no doubt, no doubt.",
+    answers: [
+      "Charles Boyle",
+      "Phil Dunphy",
+      "Jake Peralta",
+      "Stewie Griffin",
+      "Glenn Quagmire",
+      "Roger"
+    ],
+    correct: "Jake Peralta"
+  },
+  {
+    quote:
+      "I once worked with a guy for three years and never learned his name. Best friend I ever had. We still never talk sometimes.",
+    answers: [
+      "Captain Holt",
+      "Dwight Schrute",
+      "Ron Swanson",
+      "Sheldon Cooper",
+      "Charlie Sheen",
+      "Tina Belcher"
+    ],
+    correct: "Ron Swanson"
+  },
+  {
+    quote: "Any dog under fifty pounds is a cat and cats are useless.",
+    answers: [
+      "Sterling Archer",
+      "Gina Linetti",
+      "Captain Holt",
+      "Dwight Schrute",
+      "Ron Swanson",
+      "H. Wolowitz"
+    ],
+    correct: "Ron Swanson"
+  },
+  {
+    quote: "That's how you get ants!",
+    answers: [
+      "Malory Archer",
+      "Joey Tribbiani",
+      "Deadpool",
+      "Sterling Archer",
+      "Homer Simpson",
+      "Stan Marsh"
+    ],
+    correct: "Malory Archer"
+  },
+  {
+    quote: "I am the only gay in the village.",
+    answers: [
+      "Sterling Archer",
+      "Daffyd Thomas",
+      "King Arthur",
+      "Mr. Garrison",
+      "Lucius Best",
+      "Elle Woods"
+    ],
+    correct: "Daffyd Thomas"
+  },
+  {
+    quote:
+      "I’m about to do to you what Limp Bizkit did to music in the late ’90s.",
+    answers: [
+      "Stan Marsh",
+      "Mike Myers",
+      "Ron Burgundy",
+      "Deadpool",
+      "Eric Cartman",
+      "Will Ferrell"
+    ],
+    correct: "Deadpool"
+  },
+  {
+    quote:
+      "I'm wearing John Galliano but the socks are from K-Mart. As Saddam Hussein once said to me 'socks are socks - don't waste money'.",
+    answers: [
+      "Kevin Hart",
+      "Will Ferrell",
+      "Admiral Aladeen",
+      "Sterling Archer",
+      "Mr. Burns",
+      "Elle Woods"
+    ],
+    correct: "Admiral Aladeen"
+  },
+  {
+    quote:
+      "“In US and A, if you want to marry a girl, you cannot just go to her father’s house and swap her for 15 gallons of insecticide.”",
+    answers: [
+      "Deadpool",
+      "Borat",
+      "Will Ferrell",
+      "Austin Powers",
+      "Admiral Aladeen",
+      "Jake Peralta"
+    ],
+    correct: "Borat"
+  },
+  {
+    quote: "Hello, my name is ________. You killed my father. Prepare to die.",
+    answers: [
+      "Puss in Boots",
+      "Inigo Montoya",
+      "Deadpool",
+      "Bilbo Baggins",
+      "Jake Peralta",
+      "Sterling Archer"
+    ],
+    correct: "Inigo Montoya"
+  },
+  {
+    quote:
+      "Life moves pretty fast. If you don’t stop and look around once in a while, you could miss it.",
+    answers: [
+      "Shrek",
+      "Stan Marsh",
+      "Frodo Baggins",
+      "Bart Simpson",
+      "Ferris Bueller",
+      "Donkey"
+    ],
+    correct: "Ferris Bueller"
+  },
+  {
+    quote:
+      "I changed all my passwords to 'Incorrect.' So whenever I forget, it will tell me 'Your password is incorrect.'",
+    answers: [
+      "Michael Scott",
+      "Eddie Murphy",
+      "Borat",
+      "Fiona",
+      "Bart Simpson",
+      "Kim Kardashian"
+    ],
+    correct: "Michael Scott"
+  },
+  {
+    quote: "You're tearing me apart, Lisa!",
+    answers: [
+      "Johnny",
+      "Homer Simpson",
+      "Tommy Wiseau",
+      "Marge Simpson",
+      "Milhouse",
+      "Michael Scott"
+    ],
+    correct: "Johnny"
+  },
+  {
+    quote: "It’s bullshit, I did not hit her. I did nooot. Oh hi, Mark!",
+    answers: [
+      "Bart Simpson",
+      "Johnny",
+      "Kim Kardashian",
+      "Tommy Wiseau",
+      "Captain Holt",
+      "Jake Peralta"
+    ],
+    correct: "Johnny"
+  }
+];
--- main.js
+++ main.js
@@ -1,332 +1,3 @@
-//change the name to data
-let quotes = [
-  // {
-  //   quote:
-  //     "Greater good? I am your wife! I’m the greatest good you’re ever gonna get!",
-  //   answers: [
-  //     "Fiona",
-  //     "Marge Simpson",
-  //     "Maude Flanders",
-  //     "Frozones' wife",
-  //     "Cinderella",
-  //     "Kim Kardashian"
-  //   ],
-  //   correct: "Frozones' wife"
-  // },
-  {
-    quote:
-      "We get the warhead and we hold the world ransom for… One million dollars!",
-    answers: [
-      "King Arthur",
-      "Will Ferrell",
-      "Al Capone",
-      "Dr. Evil",
-      "Austin Powers",
-      "Rick Sanchez"
-    ],
-    correct: "Dr. Evil"
-  },
-  {
-    quote:
-      "It’s not a man purse. It’s called a satchel. Indiana Jones wears one.",
-    answers: [
-      "Kevin Hart",
-      "Alan Garner",
-      "Z. Galifianakis",
-      "Homer Simpson",
-      "Charlie Sheen",
-      "Deadpool"
-    ],
-    correct: "Alan Garner"
-  },
-  {
-    quote: "The fast way is to drop thermonuclear weapons over the poles.",
-    answers: [
-      "Elon Musk",
-      "The Joker",
-      "Dr. Evil",
-      "Voldemort",
-      "Freddy Krueger",
-      "Queen of Hearts"
-    ],
-    correct: "Elon Musk"
-  },
-  {
-    quote: "Keep your friends close, but your enemies closer.",
-    answers: [
-      "Hannibal Lecter",
-      "The Joker",
-      "The Godfather",
-      "Blackbeard",
-      "Cruella De Vil",
-      "Eric Cartman"
-    ],
-    correct: "The Godfather"
-  },
-  {
-    quote: "Sub Saharan, can you have 150 child warriors here by 5:00pm?",
-    answers: [
-      "Will Smith",
-      "General Hopper",
-      "Bender",
-      "Admiral Aladeen",
-      "Jack Sparrow",
-      "S. Baron Cohen"
-    ],
-    correct: "Admiral Aladeen"
-  },
-  {
-    quote:
-      "Wikipedia is the best thing ever. Anyone in the world can write anything they want about any subject. So you know you are getting the best possible information.",
-    answers: [
-      "S. Baron Cohen",
-      "Dwight Schrute",
-      "Phil Dunphy",
-      "Michael Scott",
-      "Chandler Bing",
-      "Cosmo Kramer"
-    ],
-    correct: "Michael Scott"
-  },
-  {
-    quote:
-      "Before I do anything, I ask myself, 'Would an idiot do that?' And, if the answer is yes, I do not do that thing.",
-    answers: [
-      "Dwight Schrute",
-      "Sheldon Cooper",
-      "Michael Scott",
-      "Danny DeVito",
-      "Peter Parker",
-      "Butters Scotch"
-    ],
-    correct: "Dwight Schrute"
-  },
-  {
-    quote:
-      "That's it, dishonor! Dishonor on your whole family! Dishonor on you, dishonor on your cow!",
-    answers: [
-      "Spongebob",
-      "Mushu",
-      "Frodo Baggins",
-      "Eddie Murphy",
-      "Danny DeVito",
-      "Han Solo"
-    ],
-    correct: "Mushu"
-  },
-  {
-    quote:
-      "When life gives you lemonade, make lemons. Life will be all, ‘Whaaat?’",
-    answers: [
-      "Stewie Griffin",
-      "Michael Scott",
-      "Eric Cartman",
-      "Phil Dunphy",
-      "S. Baron Cohen",
-      "Peter Griffin"
-    ],
-    correct: "Phil Dunphy"
-  },
-  {
-    quote: "Cool, cool, cool, cool, cool. No doubt, no doubt, no doubt.",
-    answers: [
-      "Charles Boyle",
-      "Phil Dunphy",
-      "Jake Peralta",
-      "Stewie Griffin",
-      "Glenn Quagmire",
-      "Roger"
-    ],
-    correct: "Jake Peralta"
-  },
-  {
-    quote:
-      "I once worked with a guy for three years and never learned his name. Best friend I ever had. We still never talk sometimes.",
-    answers: [
-      "Captain Holt",
-      "Dwight Schrute",
-      "Ron Swanson",
-      "Sheldon Cooper",
-      "Charlie Sheen",
-      "Tina Belcher"
-    ],
-    correct: "Ron Swanson"
-  },
-  {
-    quote: "Any dog under fifty pounds is a cat and cats are useless.",
-    answers: [
-      "Sterling Archer",
-      "Gina Linetti",
-      "Captain Holt",
-      "Dwight Schrute",
-      "Ron Swanson",
-      "H. Wolowitz"
-    ],
-    correct: "Ron Swanson"
-  },
-  {
-    quote: "That's how you get ants!",
-    answers: [
-      "Malory Archer",
-      "Joey Tribbiani",
-      "Deadpool",
-      "Sterling Archer",
-      "Homer Simpson",
-      "Stan Marsh"
-    ],
-    correct: "Malory Archer"
-  },
-  {
-    quote: "I am the only gay in the village.",
-    answers: [
-      "Sterling Archer",
-      "Daffyd Thomas",
-      "King Arthur",
-      "Mr. Garrison",
-      "Lucius Best",
-      "Elle Woods"
-    ],
-    correct: "Daffyd Thomas"
-  },
-  {
-    quote:
-      "I’m about to do to you what Limp Bizkit did to music in the late ’90s.",
-    answers: [
-      "Stan Marsh",
-      "Mike Myers",
-      "Ron Burgundy",
-      "Deadpool",
-      "Eric Cartman",
-      "Will Ferrell"
-    ],
-    correct: "Deadpool"
-  },
-  {
-    quote:
-      "I'm wearing John Galliano but the socks are from K-Mart. As Saddam Hussein once said to me 'socks are socks - don't waste money'.",
-    answers: [
-      "Kevin Hart",
-      "Will Ferrell",
-      "Admiral Aladeen",
-      "Sterling Archer",
-      "Mr. Burns",
-      "Elle Woods"
-    ],
-    correct: "Admiral Aladeen"
-  },
-  {
-    quote:
-      "“In US and A, if you want to marry a girl, you cannot just go to her father’s house and swap her for 15 gallons of insecticide.”",
-    answers: [
-      "Deadpool",
-      "Borat",
-      "Will Ferrell",
-      "Austin Powers",
-      "Admiral Aladeen",
-      "Jake Peralta"
-    ],
-    correct: "Borat"
-  },
-  {
-    quote: "Hello, my name is ________. You killed my father. Prepare to die.",
-    answers: [
-      "Puss in Boots",
-      "Inigo Montoya",
-      "Deadpool",
-      "Bilbo Baggins",
-      "Jake Peralta",
-      "Sterling Archer"
-    ],
-    correct: "Inigo Montoya"
-  },
-  {
-    quote:
-      "Life moves pretty fast. If you don’t stop and look around once in a while, you could miss it.",
-    answers: [
-      "Shrek",
-      "Stan Marsh",
-      "Frodo Baggins",
-      "Bart Simpson",
-      "Ferris Bueller",
-      "Donkey"
-    ],
-    correct: "Ferris Bueller"
-  },
-  {
-    quote:
-      "I changed all my passwords to 'Incorrect.' So whenever I forget, it will tell me 'Your password is incorrect.'",
-    answers: [
-      "Michael Scott",
-      "Eddie Murphy",
-      "Borat",
-      "Fiona",
-      "Bart Simpson",
-      "Kim Kardashian"
-    ],
-    correct: "Michael Scott"
-  },
-  {
-    quote: "You're tearing me apart, Lisa!",
-    answers: [
-      "Johnny",
-      "Homer Simpson",
-      "Tommy Wiseau",
-      "Marge Simpson",
-      "Milhouse",
-      "Michael Scott"
-    ],
-    correct: "Johnny"
-  },
-  {
-    quote: "It’s bullshit, I did not hit her. I did nooot. Oh hi, Mark!",
-    answers: [
-      "Bart Simpson",
-      "Johnny",
-      "Kim Kardashian",
-      "Tommy Wiseau",
-      "Captain Holt",
-      "Jake Peralta"
-    ],
-    correct: "Johnny"
-  }
-];
-
-/////////////// Randomly selected quotes /////////////////////
-
-// let randInd = Math.floor(Math.random() * data.length);
-
-// if (randInd > data.length - 10) {
-//   randInd = 0;
-// } else {
-//   randInd = randInd + 10;
-// }
-// let quotes = data.slice(randInd, 10);
-
-//////////////////////////////////////////////////////////////
-
-//Change background to random pic
-
-// let backgroundPictures = [
-//   "url('https://images.unsplash.com/photo-1452451312475-5055e48f74cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
-//   "url('https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80')",
-//   "url('https://images.unsplash.com/photo-1485095329183-d0797cdc5676?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80')",
-//   "url('https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80')"
-// ];
-
-// function randomPicture(pics) {
-//   let randomPicIndex = Math.floor(Math.random() * pics.length);
-//   return pics[randomPicIndex];
-// }
-
-// function changeBackgroundPicture() {
-//   let background = document.querySelector(".gamepage");
-//   background.style.backgroundImage = randomPicture(backgroundPictures);
-// }
-
-// setInterval(changeBackgroundPicture, 10000);
-
-/////////////////////////////////////////////////////////////////////////////
-
 let counter = 0;
 let wrongAnswers = [];
 
@@ -443,3 +114,39 @@ function wrongAnswersArr() {
     </div>`;
   document.getElementById("wrong-answers").innerHTML += result;
 }
+
+/////////////// Randomly selected quotes /////////////////////
+
+// let randInd = Math.floor(Math.random() * data.length);
+
+// if (randInd > data.length - 10) {
+//   randInd = 0;
+// } else {
+//   randInd = randInd + 10;
+// }
+// let quotes = data.slice(randInd, 10);
+
+//////////////////////////////////////////////////////////////
+
+//Change background to random pic
+
+// let backgroundPictures = [
+//   "url('https://images.unsplash.com/photo-1452451312475-5055e48f74cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+//   "url('https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80')",
+//   "url('https://images.unsplash.com/photo-1485095329183-d0797cdc5676?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80')",
+//   "url('https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80')"
+// ];
+
+// function randomPicture(pics) {
+//   let randomPicIndex = Math.floor(Math.random() * pics.length);
+//   return pics[randomPicIndex];
+// }
+
+// function changeBackgroundPicture() {
+//   let background = document.querySelector(".gamepage");
+//   background.style.backgroundImage = randomPicture(backgroundPictures);
+// }
+
+// setInterval(changeBackgroundPicture, 10000);
+
+/////////////////////////////////////////////////////////////////////////////
